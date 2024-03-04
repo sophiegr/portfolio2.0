d05a682ed63e283796aa897425044bcaa7d3e4cf
sophiegr <sogreen@mit.edu>
2024-03-04 16:14:06 -0500

    lab finished

---
+++ src/lib/Project.svelte
@@ -0,0 +1,22 @@
+<article>
+    <svelte:element this={"h" + hLevel}>{ p.title }</svelte:element>
+    <img src={p.image} alt="">
+    <p>{p.description}</p>
+</article>
+
+<script>
+    export let info = {};
+    let p = info;
+    export let hLevel = 2;
+</script>
+
+<style>
+    article {
+        display: grid;
+        grid-row: span 3;
+        grid-template-rows: subgrid;
+        h2, h3 {
+            margin: 0;
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,20 @@
 	<title>Home</title>
 </svelte:head>
 
+<script> 
+	import projects from '$lib/projects.json'; 
+	import Project from "$lib/Project.svelte";
+</script>
+
 <body>
     <h1>Sophie Green's Portfolio</h1>
     <img class='fit-picture' src="images/tree.jpg" alt="A green field with a large tree against a blue sky" />
     <p>Hello everyone. My name is Sophie Green and I am a junior at MIT. This is my portfolio website.</p>
+
+    <h2>Latest Projects:</h2>
+    <div class="projects">
+        {#each projects.slice(0, 3) as p}
+            <Project info={p} hLevel=3 />
+        {/each}
+    </div>
 </body>
